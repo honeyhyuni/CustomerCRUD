e24dda06c9be4370e2847520e7c11f9659f5b692
honeyhyuni <tjfkqwkd001@naver.com>
2022-06-23 17:39:50 +0900

    CustomerCRUD

---
+++ src/main/java/com/honeyhyuni/HoneyhyuniApplication.java
@@ -0,0 +1,13 @@
+package com.honeyhyuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneyhyuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneyhyuniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/honeyhyuni/HoneyhyuniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honeyhyuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyhyuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
